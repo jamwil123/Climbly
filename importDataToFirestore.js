5b8bb0ad47aa58f5572efddb364fbabac324d970
Szymon <szymon@MacBook-Pro-Mac-2.local>
2021-12-06 11:26:31 +0000

    importDataToFirestore.js works

---
+++ importDataToFirestore.js
@@ -0,0 +1,47 @@
+// Imports
+const firestoreService = require('firestore-export-import');
+
+
+require('dotenv').config({
+    path: './.env.dev'
+})
+
+const serviceAccount = {
+    "type": "service_account",
+    "project_id": "hills-app-test-db",
+    "private_key_id": process.env.private_key_id,
+    "private_key": process.env.private_key,
+    "client_email": process.env.client_email,
+    "client_id": process.env.private_key.client_id,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": process.env.client_x509_cert_url
+  }
+// Firebase Config
+const firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+  }
+
+// JSON To Firestore
+const jsonToFirestore = async () => {
+  try {
+    console.log('Initialzing Firebase');
+    await firestoreService.initializeApp(serviceAccount, firebaseConfig.databaseURL);
+    console.log('Firebase Initialized');
+
+    await firestoreService.restore('./data/hillsDevDB.json');
+    console.log('Upload Success');
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
+
+jsonToFirestore();
