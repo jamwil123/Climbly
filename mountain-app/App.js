de596c14e7c5b34f8cfee4cd4f16f77abd539691
Szymon <szymon@MacBook-Pro-Mac-2.local>
2021-12-13 23:03:41 +0000

    Sorting feature sorted and styled

--- mountain-app/App.js
+++ mountain-app/App.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import { StyleSheet, Button, Modal, View, Text, Dimensions } from "react-native";
+import { StyleSheet, Button, Modal, View, Text, Dimensions, Pressable, TouchableHighlight } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Header from "./src/components/Header";
@@ -8,6 +8,7 @@ import Footer from "./src/components/Footer";
 import SingleMountain from "./src/components/SingleMountain";
 import UserMain from "./src/components/UserMain";
 import { userContext } from "./src/contexts/userContext";
+import { TouchableOpacity } from "react-native-web";
 // import {FontAwesome} from "@expo/vector-icons"
 // import {Icon} from 'react-native-elements';
 
@@ -33,12 +34,16 @@ export default function App() {
             options={{
               headerTitle: "Mountains",
               headerLeft: () => (
-                <Button
-                  title="Sort ->"
+                <Pressable
+                  style={styles.sortButton}
                   onPress={() => {
                     setModalVisible(true);
                   }}
-                />
+                >
+                  <Text style={styles.sortButtonText}>
+                    Sort ->
+                  </Text>
+                </Pressable>
               ),
             }}
           >
@@ -55,41 +60,67 @@ export default function App() {
             visible={modalVisible}
           >
             <View style={styles.modalView}>
-              <Text>Sort mountains by:</Text>
-              <Button
-                title="x"
-                onPress={() => {
-                  setModalVisible(false);
-                }}
-              ></Button>
-              <Button
-                title="Name a-z"
-                onPress={() => {
-                  setSortQuery({ sort: "hillname", order: "ASC" });
-                  setModalVisible(false);
-                }}
-              ></Button>
-              <Button
-                title="Name z-a"
-                onPress={() => {
-                  setSortQuery({ sort: "hillname", order: "DESC" });
-                  setModalVisible(false);
-                }}
-              ></Button>
-              <Button
-                title="Feet hight to low"
-                onPress={() => {
-                  setSortQuery({ sort: "feet", order: "DESC" });
-                  setModalVisible(false);
-                }}
-              ></Button>
-              <Button
-                title="Feet low to hight"
-                onPress={() => {
-                  setSortQuery({ sort: "feet", order: "ASC" });
-                  setModalVisible(false);
-                }}
-              ></Button>
+              <Text style={styles.modalText}>Sort mountains by:</Text>
+              <TouchableHighlight
+                  underlayColor={0x6F6DABff}
+                  onPressOut={() => {
+                    setSortQuery({ sort: "hillname", order: "ASC" });
+                    setModalVisible(false);
+                  }}
+                  style={styles.modalButton}
+              >
+                    <Text style={styles.modalButtonText}>
+                    Name a-z
+                    </Text>
+              </TouchableHighlight>
+              <TouchableHighlight
+                  underlayColor={0x6F6DABff}
+                  onPressIn={(e) => {console.log(e.target)}}
+                  onPress={() => {
+                    setSortQuery({ sort: "hillname", order: "DESC" });
+                    setModalVisible(false);
+                  }}
+                  style={styles.modalButton}
+              >
+                    <Text style={styles.modalButtonText}>
+                    Name z-a
+                    </Text>
+              </TouchableHighlight>
+              <TouchableHighlight
+                  underlayColor={0x6F6DABff}
+                  onPress={() => {
+                    setSortQuery({ sort: "feet", order: "DESC" });
+                    setModalVisible(false);
+                  }}
+                  style={styles.modalButton}
+              >
+                    <Text style={styles.modalButtonText}>
+                    Feet hight to low
+                    </Text>
+              </TouchableHighlight>
+              <TouchableHighlight
+                  underlayColor={0x6F6DABff}
+                  onPress={() => {
+                    setSortQuery({ sort: "feet", order: "ASC" });
+                    setModalVisible(false);
+                  }}
+                  style={styles.modalButton}
+              >
+                    <Text style={styles.modalButtonText}>
+                    Feet low to hight
+                    </Text>
+              </TouchableHighlight>
+              <TouchableHighlight
+                  underlayColor='0x2e2d4dff'
+                  onPress={() => {
+                    setModalVisible(false);
+                  }}
+                  style={styles.modalExitButton}
+              >
+                    <Text style={styles.modalButtonText}>
+                    X
+                    </Text>
+              </TouchableHighlight>
             </View>
           </Modal>
         </View>
@@ -108,9 +139,24 @@ const styles = StyleSheet.create({
   middlesection: {
     flex: 8,
   },
+  sortButton: {
+    borderColor: 0x2e2d4dff,
+    backgroundColor: 0xDDDDF0FF,
+    borderWidth: 1,
+    borderRadius: 15,
+    paddingLeft: 12,
+    paddingRight: 12,
+    paddingTop: 5,
+    paddingBottom: 5
+  },
+  sortButtonText: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    fontSize: 17
+  },
   modalView: {
     marginTop: "auto",
-    marginBottom: 50,
+    marginBottom: 0,
     backgroundColor: "white",
     borderRadius: 20,
     padding: 35,
@@ -124,10 +170,28 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
     elevation: 5,
   },
+  modalText: {
+    fontSize: 30,
+    marginBottom: 18
+  },
   outterView: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
     marginTop: 500,
   },
+  modalButton: {
+    width: screenSize.width * 0.8,
+    backgroundColor: 0xDDDDF0FF,
+    borderColor: 0x2e2d4dff,
+    borderWidth: 1,
+    borderRadius: 20,
+    marginBottom: 12,
+    padding: 6,
+  },
+  modalButtonText: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    fontSize: 22
+  }
 });
