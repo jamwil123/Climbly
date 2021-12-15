b3f84d3d4c22482a1d91c3d29413d9dc43bd7a8c
back-jond1992 <b.jond7@outlook.com>
2021-12-15 16:11:49 +0000

    changes footer to add search

--- mountain-app/src/components/Footer.js
+++ mountain-app/src/components/Footer.js
@@ -1,51 +1,66 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity, Image} from 'react-native';
-import { Ionicons } from '@expo/vector-icons';  
-import { FontAwesome } from '@expo/vector-icons'; 
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { ModalSearch } from "./ModalSearch";
 
-
-const Footer = ({navigationRef}) => {
-    const [home, setHome] = useState(true)
-    const [user, setUser] = useState(false)
-    return (
-        <TouchableOpacity onPress={() => {setHome(!home)}}>
-        <View style={styles.footer}>
-<Ionicons name="home" size={40} color={'black'} style={styles.homebutton} onPress={() => {
-    setUser(true)
-    navigationRef.current.navigate("Mountain App")
-    
-    
-}} />
-<FontAwesome name="user" size={40} color={'black'} style={styles.userbutton} onPress={() => {
-    setHome(false)
-    navigationRef.current.navigate("UserPage")
-}}/>
-</View>
-</TouchableOpacity>
-    );
+const Footer = ({ navigationRef }) => {
+  return (
+    <TouchableOpacity onPress={() => {}}>
+      <View style={styles.footer}>
+        <Ionicons
+          name="home"
+          size={40}
+          color={"black"}
+          style={styles.homebutton}
+          onPress={() => {
+            navigationRef.current.navigate("Mountain App");
+          }}
+        />
+        <FontAwesome
+          name="search"
+          size={40}
+          color={"black"}
+          style={styles.userbutton}
+          onPress={() => {
+            navigationRef.current.navigate("ModalSearch");
+          }}
+        />
+        <FontAwesome
+          name="user"
+          size={40}
+          color={"black"}
+          style={styles.userbutton}
+          onPress={() => {
+            navigationRef.current.navigate("UserPage");
+          }}
+        />
+      </View>
+    </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
-    footer: {
-        backgroundColor: 0xAAADC4FF,
-        width: '100%',
-        height: 75,
-        display: "flex",
-        flexDirection: 'row'
-    },homebutton: {
-        justifyContent: 'flex-start',
-        flex: 1,
-        paddingLeft:40,
-        paddingTop: 15, 
-        paddingRight: 40
-    },
-    userbutton: {
-        justifyContent: 'flex-end',
-        textAlign: "right",
-        paddingRight : 40,
-        paddingTop: 17
-    }
-
-  });
+  footer: {
+    backgroundColor: 0xaaadc4ff,
+    width: "100%",
+    height: 75,
+    display: "flex",
+    flexDirection: "row",
+  },
+  homebutton: {
+    justifyContent: "flex-start",
+    flex: 1,
+    paddingLeft: 40,
+    paddingTop: 15,
+    paddingRight: 40,
+  },
+  userbutton: {
+    justifyContent: "flex-end",
+    textAlign: "right",
+    paddingRight: 40,
+    paddingTop: 17,
+  },
+});
 
 export default Footer;
