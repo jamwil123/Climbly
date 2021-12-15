542e380ef46bdadca06c1eca0409c37e7b957ffb
myangan <muugii85@hotmail.com>
2021-12-15 22:04:58 +0000

    userCompleteMountain has being adjasted and no longer can be pressed

--- mountain-app/src/components/UsersCompleteMountain.js
+++ mountain-app/src/components/UsersCompleteMountain.js
@@ -7,22 +7,16 @@ import HillCard from "./HillCard.js";
 export default function UsersCompleteMountain() {
   const { currentUser } = useContext(userContext);
   const HillCardRef = React.useRef();
+  console.log(currentUser);
   return (
     <View style={styles.cardViews}>
       <FlatList
         data={currentUser.hillsClimbed}
         renderItem={({ item }) => {
           return (
-            <TouchableHighlight
-              onPress={() => {
-                navigation.push("SingleMountainPage", { mountain: item });
-              }}
-              underlayColor="white"
-            >
-              <View ref={HillCardRef}>
-                <HillCard key={item.hillnumber} hillObject={item} />
-              </View>
-            </TouchableHighlight>
+            <View>
+              <HillCard key={item.hillnumber} hillObject={item} />
+            </View>
           );
         }}
         keyExtractor={(item) => {
